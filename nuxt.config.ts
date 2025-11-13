1df50e7a182732c3e3ed5b98250958c93fa91437
Artus <artuslama@gmail.com>
2025-11-13 17:59:34 +0100

    📦Add ESLint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: [
-    '@nuxt/icon',
-    '@nuxt/hints',
-    '@nuxtjs/html-validator',
-    '@nuxt/image',
-    '@nuxt/eslint'
-  ]
+    compatibilityDate: "2025-07-15",
+    devtools: { enabled: true },
+    modules: [
+        "@nuxt/icon",
+        "@nuxt/hints",
+        "@nuxtjs/html-validator",
+        "@nuxt/image",
+        "@nuxt/eslint",
+    ],
+
+    eslint: {
+        config: {
+            stylistic: true,
+        },
+    },
 })
