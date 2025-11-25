6f46bceb9dcd4fb3e5a8ce5986164e8a1dafc850
Artus <artuslama@gmail.com>
2025-11-25 15:04:42 +0100

    ✨Add basic FAQ components

---
+++ app/components/app/faq/Accordion.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const { items } = defineProps<{
+    items: {
+        question: string
+        answer: string
+    }[]
+}>()
+</script>
+
+<template>
+    <UiAccordion collapsible>
+        <UiAccordionItem
+            v-for="(item, index) in items"
+            :key="index"
+            :value="`item-${index}`"
+            class="border-b last:border-b-0"
+        >
+            <UiAccordionTrigger>
+                {{ item.question }}
+            </UiAccordionTrigger>
+            <UiAccordionContent>
+                {{ item.answer }}
+            </UiAccordionContent>
+        </UiAccordionItem>
+    </UiAccordion>
+</template>
--- app/pages/faq.vue
+++ app/pages/faq.vue
@@ -1,5 +1,26 @@
+<script setup lang="ts">
+const faqItems = [
+    {
+        question: "Example Question 1?",
+        answer: "Answer to the example question 1. And random additional text to make it longer.",
+    },
+    {
+        question: "Example Question 2?",
+        answer: "Answer to the example question 2. And random additional text to make it longer.",
+    },
+    {
+        question: "Example Question 3?",
+        answer: "Answer to the example question 3. And random additional text to make it longer.",
+    },
+    {
+        question: "Example Question 4?",
+        answer: "Answer to the example question 4. And random additional text to make it longer.",
+    },
+]
+</script>
+
 <template>
-    <div>
-        <p>nothing here yet</p>
+    <div class="w-full">
+        <AppFaqAccordion :items="faqItems" />
     </div>
 </template>
