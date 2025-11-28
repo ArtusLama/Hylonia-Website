da3e9ec1c38fe1dc3598d49f584a930a21fab2f7
Artus <artuslama@gmail.com>
2025-11-28 19:25:22 +0100

    🐛fix page content center and width

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="overflow-x-hidden">
+    <div class="overflow-x-hidden flex flex-col items-center">
         <NavigationNavBar />
-        <div class="pt-32 mx-32 flex justify-center min-h-screen">
+        <div class="pt-32 mx-32 flex justify-center min-h-screen w-7xl">
             <slot />
         </div>
         <Footer />
