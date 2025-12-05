7cedf0a811dc8f8bbabfed7beb3ffc27a2676344
Artus <artuslama@gmail.com>
2025-12-05 21:02:20 +0100

    🎨Improve rules section padding

--- app/components/app/rules/SectionBase.vue
+++ app/components/app/rules/SectionBase.vue
@@ -5,15 +5,15 @@ defineProps<{
 </script>
 
 <template>
-    <div class="border p-4 rounded-lg space-y-4">
+    <div class="border px-10 py-8 rounded-lg space-y-10">
+        <div class="prose dark:prose-invert">
+            <slot />
+        </div>
         <p
             v-if="lastUpdated"
             class="text-sm text-muted-foreground"
         >
             Letzte Änderung am {{ lastUpdated }}
         </p>
-        <div class="prose dark:prose-invert">
-            <slot />
-        </div>
     </div>
 </template>
